70a5d1c29a39e1274d545c0e6bca84f10573821d
Vitaly <kaliummati@gmail.com>
2021-05-01 12:32:59 +0900

    настроен seed для слов и внесены небольшие правки

---
+++ prisma/seed.js
@@ -0,0 +1,21 @@
+// нужды в компилировании этого файл нет, как и в типизации тут 
+const { PrismaClient } = require('@prisma/client');
+const fs = require('fs');
+
+const prisma = new PrismaClient();
+
+(async () => {
+    const words = fs.readFileSync('./words.docx', {encoding: 'utf8'})
+        .split('\n')
+        .filter((string) => string.length > 0);
+
+    for (word of words) {
+        // createMany с sqlite не работает, потому - постреялем
+        await prisma.word.create({data: {content: word, length: word.length}});
+    }
+}).catch(e => {
+    console.error(e)
+    process.exit(1)
+}).finally(async () => {
+    await prisma.$disconnect()
+});
